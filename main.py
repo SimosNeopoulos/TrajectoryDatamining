3c3f39ec35e84be4a9eeb3b26da3a678bf6f3bcd
SimosNeopoulos <simneo22@gmail.com>
2023-12-19 17:48:22 +0200

    finished distance and cos similarity.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import data_handler as dh
 import similarity_mesures as sm
 
-path_to_trajectory = r'C:\Trajectory_Generator\generator_v2\oldenberg.dat'
+path_to_trajectory = r'C:\Trajectory_Generator\generator_v2\oldenburg.dat'
 # path_to_trajectory = r'C:\Trajectory_Generator\generator_v2\tl_2017_06075_edges.dat'
 oldenburg_json_path = r'.\data_files\oldenburg_data.json'
 tl_json_path = r'.\data_files\tl_data.json'
@@ -21,19 +21,41 @@ tl_pairs_path = r'.\data_files\tl_data.pkl'
 
 
 def main():
-    print(':)')
+    # print(':)')
 
     # graph, node_index, embedding_dict = dh.create_graph_and_index(
     #     path_to_trajectory)
     # dh.save_graph_data(graph, node_index, embedding_dict,
     #                 oldenburg_json_path)
-    # graph, node_index, embedding_dict = dh.load_graph_data(
-    #     tl_json_path)
+    graph, node_index, embedding_dict = dh.load_graph_data(oldenburg_json_path)
     # dh.save_graph_pairs(graph, tl_pairs_path)
-    # pair_dict = dh.load_graph_pairs(oldenburg_pairs_path)
+    pair_dict = dh.load_graph_pairs(oldenburg_pairs_path)
 
+    trajectory_catalog = dh.get_trajectory_catalog(path_to_trajectory)
 
-    # trajectory_catalog = get_trajectory_catalog(path_to_trajectory)
+    distance_similarity = sm.k_distance_similarity(
+        trajectory_catalog[0], trajectory_catalog, node_index, pair_dict, 100
+    )
+
+    cos_similarity = sm.k_cosine_similarity(
+        trajectory_catalog[0], trajectory_catalog, embedding_dict,
+        node_index, 100
+    )
+
+    set1 = set()
+    set2 = set()
+    for index, (dist_sim, cos_sim) in enumerate(
+            zip(distance_similarity, cos_similarity)):
+        (dist_id1, dist_id2), dist_s = dist_sim
+        (cos_id1, cos_id2), cos_s = cos_sim
+        print(f'{index + 1}) Dist similarity: nodes {dist_id1}-{dist_id2} with'
+              f' {dist_s:.4f} | Cos similarity: nodes {cos_id1}-{cos_id2} with'
+              f' {cos_s:.4f}')
+        set1.add((dist_id1, dist_id2))
+        set2.add((cos_id1, cos_id2))
+
+    print(len(list(set1.intersection(set2))))
+
     #
     # print(k_similar_trajectories(trajectory_catalog[0], trajectory_catalog, 5))
 
--- similarity_mesures.py
+++ similarity_mesures.py
@@ -7,7 +7,7 @@ from itertools import zip_longest
 def get_trajectory_vector(trajectory, embedding_dict, node_index):
     trajectory_vector = []
     for point in trajectory.trajectory_path:
-        point_vector = embedding_dict[node_index[point.node_id]]
+        point_vector = embedding_dict[str(node_index[point.node_id])]
         trajectory_vector = [a + b for a, b in
                              zip_longest(trajectory_vector, point_vector,
                                          fillvalue=0)]
@@ -24,6 +24,49 @@ def get_cos_similarity(trajectory1, trajectory2, embedding_dict, node_index):
 
 def cosine_similarity(vector1, vector2):
     return dot(vector1, vector2) / (norm(vector1) * norm(vector2))
+
+
+def k_cosine_similarity(main_trajectory, trajectories, embedding_dict,
+                        node_index, k):
+
+    similarity_list = []
+    for trajectory in trajectories.values():
+        similarity = get_cos_similarity(
+            main_trajectory, trajectory, embedding_dict, node_index
+        )
+        similarity_list.append(
+            ((main_trajectory.id, trajectory.id), similarity))
+    return sorted(similarity_list, key=lambda x: x[1], reverse=True)[:k]
+
+
+def get_distance_similarity(trajectory1, trajectory2, node_dict, pair_dist):
+    distance12 = calculate_node_distance(trajectory1, trajectory2, node_dict,
+                                         pair_dist)
+    distance21 = calculate_node_distance(trajectory2, trajectory1, node_dict,
+                                         pair_dist)
+    return (distance12 + distance21) / 2
+
+
+def k_distance_similarity(main_trajectory, trajectories, node_dict, pair_dist,
+                          k):
+    similarity_list = []
+    for trajectory in trajectories.values():
+        similarity = get_distance_similarity(
+            main_trajectory, trajectory, node_dict, pair_dist
+        )
+        similarity_list.append(
+            ((main_trajectory.id, trajectory.id), similarity))
+    return sorted(similarity_list, key=lambda x: x[1])[:k]
+
+
+def calculate_node_distance(trajectory1, trajectory2, node_dict, pair_dist):
+    distance_list = []
+    for node1 in trajectory1.trajectory_path:
+        distance_list.append(min([pair_dist[node_dict[node1.node_id]]
+                                  [node_dict[node2.node_id]]
+                                  for node2 in trajectory2.trajectory_path]))
+
+    return sum(distance_list) / len(trajectory1.trajectory_path)
 
 
 def get_jaccard_similarity(trajectory1, trajectory2):
